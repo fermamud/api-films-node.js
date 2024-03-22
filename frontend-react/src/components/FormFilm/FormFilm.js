a1408769dce7b80430d10a9e61616def2dd4fd48
Frata Mamud <e2395117@cmaisonneuve.qc.ca>
2024-03-22 14:48:44 -0400

    Cours 18. insertion d'image e gestion de boit a cocher

--- frontend-react/src/components/FormFilm/FormFilm.js
+++ frontend-react/src/components/FormFilm/FormFilm.js
@@ -1,9 +1,22 @@
 // import { Form } from "react-router-dom";
 import './FormFilm.css';
 import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
 // baixar react developer tools
 
 function FormFilm() {
+    const genres = [
+        "Action",
+        "Aventure",
+        "Comédie",
+        "Drame",
+        "Fantaisie",
+        "Horreur",
+        "Policier",
+        "Science-fiction",
+        "Thriller",
+        "Western"
+    ];
 
     const [formData, setFormData] = useState({
         titre: "",
@@ -16,19 +29,43 @@ function FormFilm() {
 
 
     const [formValidity, setFormValidity] = useState("invalid");
+    // const navigate = useNavigate();
     // ICI ON POURRAIT UTILISER USESTATE POUR GERER LA MESSAGE D'ERREUR (VER O CODIGO DELE)
 
     function onFormDataChange(evenement) {
         const name = evenement.target.name;
         const value = evenement.target.value;
         // const {name, value} = evenement.target;
-        
-        const estValide = evenement.target.form.checkValidity() ? "valid" : "invalid";
-        setFormValidity(estValide);
 
-        // On clone la donnee dans un nouvel objet
-        const donneeModifiee = {...formData, [name]:value};
-        setFormData(donneeModifiee);
+        if (name.startsWith("genre")) {
+            const estCoche = evenement.target.checked;
+            let genres = formData.genres || [];
+
+            // Si on decoche, on enleve
+            if (!estCoche && genres.includes(value)) {
+                genres = genres.filter((genre, index) => {
+                    return genre !== value;
+                })
+            } else if (estCoche && !genres.includes(value)) {
+                genres.push(value);
+            }
+            const donneeModifiee = {...formData, "genres":genres};
+            setFormData(donneeModifiee);
+        } else if (name == "titreVignette") {
+            const nomFichier = evenement.target.files[0].name;
+            // como ele fez na aula
+            // const donneeModifiee = {...formData, titreVignette: nomFichier};
+            const donneeModifiee = {...formData, [name]:nomFichier}; //teste Fernanda
+            setFormData(donneeModifiee);
+        } else {
+            const estValide = evenement.target.form.checkValidity() ? "valid" : "invalid";
+            setFormValidity(estValide);
+            
+            // On clone la donnee dans un nouvel objet
+            const donneeModifiee = {...formData, [name]:value};
+            setFormData(donneeModifiee);
+        }
+            
     }
 
     async function onFormSubmit(evenement) {
@@ -74,6 +111,7 @@ function FormFilm() {
 
             // Reinit l'état de validité
             setFormValidity("invalid");
+            // navigate("/"); // Redirige vers une page en particulier
         } else {
             const messageErreur = response.message;
             console.log("erreur", messageErreur);
@@ -101,6 +139,32 @@ function FormFilm() {
                         <label htmlFor="annee">Annee</label>
                         <input type="text" id="annee" name="annee" value={formData.annee} onChange={onFormDataChange} required minLength="1" maxLength="4"></input>
                     </div>
+
+
+                    <div className="input-group">
+                        <p>Genres</p>
+                        {
+                            genres.map((element, index) => {
+                                return (
+                                    <div key={index}>
+                                        <input type="checkbox" id={element} name={`genre-${element}`} value={element} onChange={onFormDataChange} checked={formData.genres.includes(element)} />
+                                        <label htmlFor={element}>{element}</label>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
+
+
+                    <div className="input-group">
+                        <label htmlFor="titreVignette">Vignette</label>
+                        <input type="file" name="titreVignette" id="titreVignette" accept=".jpeg, .jpg, .png, .webp" onChange={onFormDataChange}/>
+                    </div>
+
+
+
+
                     <input type="submit" value="Envoyer" disabled={ formValidity === "invalid" ? "disabled" : ""}></input>
                 </form>
                 {/* olhar no codigo dele a continuacao
